307b611d94124f8e7223f0fcd68641484ecc7921
Ben Meier <benmeier@fastmail.com>
2015-11-17 17:15:19 +0200

    tested content of written archive

--- tests/com/bunkr_beta_tests/scenarios/TestWriteScenarios.java
+++ tests/com/bunkr_beta_tests/scenarios/TestWriteScenarios.java
@@ -3,16 +3,21 @@ package com.bunkr_beta_tests.scenarios;
 import com.bunkr_beta.*;
 import com.bunkr_beta.interfaces.IArchiveInfoContext;
 import com.bunkr_beta.inventory.FileInventoryItem;
+import com.bunkr_beta.inventory.Inventory;
 import com.bunkr_beta.streams.MultilayeredOutputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.security.NoSuchAlgorithmException;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -54,8 +59,40 @@ public class TestWriteScenarios
         }
         MetadataWriter.write(context);
 
-        System.out.println(tempfile);
-        Files.copy(tempfile.toPath(), new File("./scenario_one.bunkr").toPath());
+        try(DataInputStream dis = new DataInputStream(new FileInputStream(tempfile)))
+        {
+            assertEquals(IO.readNByteString(dis, 5), "BUNKR");
+            assertEquals(dis.read(), 0);
+            assertEquals(dis.read(), 0);
+            assertEquals(dis.read(), 1);
+            assertEquals(dis.readInt(), 1024);
+            assertEquals(dis.readLong(), 4096);
+            byte[] data = new byte[4096];
+            dis.read(data);
+            for (int i = 0; i < 3333; i++)
+            {
+                assertEquals(data[i], (65 + i % 26));
+            }
+            String invJSON = IO.readNByteString(dis, dis.readInt());
+            Inventory inventory = new ObjectMapper().readValue(invJSON, Inventory.class);
+            assertEquals(inventory.files.size(), 1);
+            assertEquals(inventory.folders.size(), 0);
+            assertEquals(inventory.files.get(0).getName(), "some file.txt");
+            assertEquals(inventory.files.get(0).getBlocks().toString(), "FragmentedRange{0,1,2,3,}");
+            assertEquals(inventory.files.get(0).getUuid(), newFile.getUuid());
+            assertEquals(inventory.files.get(0).getSize(), newFile.getSize());
+            assertEquals(inventory.files.get(0).getModifiedAt(), newFile.getModifiedAt());
+            assertArrayEquals(inventory.files.get(0).getEncryptionIV(), newFile.getEncryptionIV());
+            assertArrayEquals(inventory.files.get(0).getEncryptionKey(), newFile.getEncryptionKey());
+
+            String desJSON = IO.readNByteString(dis, dis.readInt());
+            Descriptor descriptor = new ObjectMapper().readValue(desJSON, Descriptor.class);
+
+            assertEquals(descriptor.compression, null);
+            assertEquals(descriptor.encryption, null);
+            
+            assertEquals(dis.available(), 0);
+        }
     }
 
 
