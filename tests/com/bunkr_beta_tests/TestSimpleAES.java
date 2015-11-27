525426a3ff9437ae66e27b03b47d784931d345ee
Ben Meier <benmeier@fastmail.com>
2015-11-28 00:46:48 +0200

    Added simpleaes test for empty string

--- tests/com/bunkr_beta_tests/TestSimpleAES.java
+++ tests/com/bunkr_beta_tests/TestSimpleAES.java
@@ -16,13 +16,28 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class TestSimpleAES
 {
     @Test
-    public void testMain() throws CryptoException
+    public void testSimple() throws CryptoException
     {
         byte[] k = RandomMaker.get(256);
         byte[] iv = RandomMaker.get(256);
 
         byte[] p = ("'Ha, Watson! It would appear that our bait, cast though it was over unknown waters, may have " +
                     "brought in a catch!'").getBytes();
+
+        byte[] c = SimpleAES.encrypt(p, k, iv);
+
+        byte[] d = SimpleAES.decrypt(c, k, iv);
+
+        assertThat(p, is(equalTo(d)));
+    }
+
+    @Test
+    public void testNoPaddingRequired() throws CryptoException
+    {
+        byte[] k = RandomMaker.get(256);
+        byte[] iv = RandomMaker.get(256);
+
+        byte[] p = ("").getBytes();
 
         byte[] c = SimpleAES.encrypt(p, k, iv);
 
