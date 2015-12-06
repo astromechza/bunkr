fdcea265a7094e5a0a6525d735fe88afe0e0f523
Ben Meier <benmeier@fastmail.com>
2015-12-06 21:35:31 +0200

    added better coverage for rmdir

--- tests/com/bunkr_beta_tests/commands/TestRmdirCommand.java
+++ tests/com/bunkr_beta_tests/commands/TestRmdirCommand.java
@@ -1,6 +1,5 @@
 package com.bunkr_beta_tests.commands;
 
-import com.bunkr_beta.cli.commands.MkdirCommand;
 import com.bunkr_beta.cli.commands.RmdirCommand;
 import com.bunkr_beta.exceptions.TraversalException;
 import com.bunkr_beta.inventory.FileInventoryItem;
@@ -12,7 +11,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static junit.framework.TestCase.fail;
-import static org.junit.Assert.assertFalse;
 
 /**
  * Creator: benmeier
@@ -23,76 +21,110 @@ public class TestRmdirCommand
     public Inventory makeSampleInventory()
     {
         Inventory i = new Inventory(new ArrayList<>(), new ArrayList<>());
-        FolderInventoryItem d1 = new FolderInventoryItem("d1");
+        FolderInventoryItem d1 = new FolderInventoryItem("t1");
         i.getFolders().add(d1);
-        FolderInventoryItem d2 = new FolderInventoryItem("d2");
-        i.getFolders().add(d2);
-        FolderInventoryItem d3 = new FolderInventoryItem("d2.d3");
-        d2.getFolders().add(d3);
 
-        FileInventoryItem f1 = new FileInventoryItem("f1");
-        i.getFiles().add(f1);
-        FileInventoryItem f2 = new FileInventoryItem("f2");
-        i.getFiles().add(f2);
-        FileInventoryItem f3 = new FileInventoryItem("d2.f3");
-        d2.getFiles().add(f3);
+        FolderInventoryItem d2 = new FolderInventoryItem("t2");
+        i.getFolders().add(d2);
+        d2.getFiles().add(new FileInventoryItem("file"));
+
+        FolderInventoryItem d3 = new FolderInventoryItem("t3");
+        i.getFolders().add(d3);
+        d3.getFolders().add(new FolderInventoryItem("file"));
+
+        FileInventoryItem t4 = new FileInventoryItem("t4");
+        i.getFiles().add(t4);
 
         return i;
     }
 
     @Test
-    public void testRmdirSingleEmpty() throws TraversalException
+    public void testRmdirEmpty() throws TraversalException
     {
         Inventory inv = makeSampleInventory();
-        InventoryPather.traverse(inv, "/d1").isAFolder();
-        new RmdirCommand().rmdir(inv, "/d1", false);
+        InventoryPather.traverse(inv, "/t1").isAFolder();
+        new RmdirCommand().rmdir(inv, "/t1", false);
         try
         {
-            InventoryPather.traverse(inv, "/d1");
+            InventoryPather.traverse(inv, "/t1");
             fail("did not through traversal exception");
         }
         catch(TraversalException ignored) {}
     }
 
     @Test
-    public void testRmdirSingleNonEmpty() throws TraversalException
+    public void testRmdirNonEmptyWithFolder() throws TraversalException
     {
         Inventory inv = makeSampleInventory();
-        InventoryPather.traverse(inv, "/d2").isAFolder();
+        InventoryPather.traverse(inv, "/t2").isAFolder();
         try
         {
-            new RmdirCommand().rmdir(inv, "/d2", false);
+            new RmdirCommand().rmdir(inv, "/t2", false);
             fail("did not through traversal exception");
         }
         catch(TraversalException ignored) {}
-        InventoryPather.traverse(inv, "/d2").isAFolder();
-        new RmdirCommand().rmdir(inv, "/d2", true);
+        InventoryPather.traverse(inv, "/t2").isAFolder();
+        new RmdirCommand().rmdir(inv, "/t2", true);
         try
         {
-            InventoryPather.traverse(inv, "/d2");
+            InventoryPather.traverse(inv, "/t2");
             fail("did not through traversal exception");
         }
         catch(TraversalException ignored) {}
-
     }
 
     @Test
-    public void testRmdirLevelTwoEmpty() throws TraversalException
+    public void testRmdirNonEmptyWithFile() throws TraversalException
     {
         Inventory inv = makeSampleInventory();
-        InventoryPather.traverse(inv, "/d2/d2.d3").isAFolder();
-        new RmdirCommand().rmdir(inv, "/d2/d2.d3", false);
+        InventoryPather.traverse(inv, "/t3").isAFolder();
         try
         {
-            InventoryPather.traverse(inv, "/d2/d2.d3");
+            new RmdirCommand().rmdir(inv, "/t3", false);
             fail("did not through traversal exception");
         }
         catch(TraversalException ignored) {}
-
+        InventoryPather.traverse(inv, "/t3").isAFolder();
+        new RmdirCommand().rmdir(inv, "/t3", true);
+        try
+        {
+            InventoryPather.traverse(inv, "/t3");
+            fail("did not through traversal exception");
+        }
+        catch(TraversalException ignored) {}
     }
 
+    @Test
+    public void testRmdirAFile() throws TraversalException
+    {
+        Inventory inv = makeSampleInventory();
+        InventoryPather.traverse(inv, "/t4").isAFile();
+        try
+        {
+            new RmdirCommand().rmdir(inv, "/t4", false);
+        }
+        catch(TraversalException ignored) {}
+    }
 
+    @Test
+    public void testRmdirInAFile() throws TraversalException
+    {
+        Inventory inv = makeSampleInventory();
+        try
+        {
+            new RmdirCommand().rmdir(inv, "/t4/t4", false);
+        }
+        catch(TraversalException ignored) {}
+    }
 
-
-
+    @Test
+    public void testRmdirRoot()
+    {
+        Inventory inv = makeSampleInventory();
+        try
+        {
+            new RmdirCommand().rmdir(inv, "/", true);
+        }
+        catch(TraversalException ignored) {}
+    }
 }
